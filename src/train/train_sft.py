c3cd37c7a413c38df20ddeb28445f64d733ac6eb
DOPHANK <93715124+DOPHANK@users.noreply.github.com>
2025-06-12 10:29:59 +0700

    Update train_sft.py

--- src/train/train_sft.py
+++ src/train/train_sft.py
@@ -64,10 +64,6 @@ def train():
         (ModelArguments, DataArguments, TrainingArguments))
     
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
-    print(model_args)
-    print(data_args)
-    print("data_args.data_path =", data_args.data_path)
-    print(training_args)
     
     use_liger = training_args.use_liger
     if "Qwen2.5" in model_args.model_id:
