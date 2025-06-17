b515509dc0eb30e6a8683259b96d42568a123f5a
DOPHANK <93715124+DOPHANK@users.noreply.github.com>
2025-06-17 11:44:45 +0700

    Update train_sft.py

--- src/train/train_sft.py
+++ src/train/train_sft.py
@@ -192,7 +192,7 @@ def train():
     # Debug: Print trainable parameters
     trainable_params = [(n, p.dtype) for n, p in model.named_parameters() if p.requires_grad]
     
-    print(f"\n[INFO] After *processor = AutoProcessor.from_pretrained(model_args.model_id)* Trainable parameter count: {len(trainable_params)}")
+    print(f"\n[INFO] After processor Trainable parameter count: {len(trainable_params)}")
     
     # model.config.tokenizer_model_max_length = processor.tokenizer.model_max_length
 
@@ -213,19 +213,7 @@ def train():
     # Debug: Print trainable parameters
     trainable_params = [(n, p.dtype) for n, p in model.named_parameters() if p.requires_grad]
     
-    print(f"\n[INFO] After *if training_args.bits in [4, 8]:
-        from peft.tuners.lora import LoraLayer
-        for name, module in model.named_modules():
-            if isinstance(module, LoraLayer):
-                if training_args.bf16:
-                    module = module.to(torch.bfloat16)
-            if 'norm' in name:
-                module = module.to(torch.float32)
-            
-            if 'lm_head' in name or 'embed_token' in name:
-                if hasattr(module, 'weight'):
-                    if training_args.bf16 and module.weight.dtype == torch.float32:
-                        module = module.to(torch.bfloat16)* Trainable parameter count: {len(trainable_params)}")
+    print(f"\n[INFO] After if training_args.bits in [4, 8] Trainable parameter count: {len(trainable_params)}")
 
     data_module = make_supervised_data_module(model_id=model_args.model_id,
                                               processor=processor,
@@ -234,9 +222,7 @@ def train():
     # Debug: Print trainable parameters
     trainable_params = [(n, p.dtype) for n, p in model.named_parameters() if p.requires_grad]
     
-    print(f"\n[INFO] After *data_module = make_supervised_data_module(model_id=model_args.model_id,
-                                              processor=processor,
-                                              data_args=data_args)* Trainable parameter count: {len(trainable_params)}")
+    print(f"\n[INFO] After data_module Trainable parameter count: {len(trainable_params)}")
   
 
     trainer = QwenSFTTrainer(
@@ -249,12 +235,7 @@ def train():
      # Debug: Print trainable parameters
     trainable_params = [(n, p.dtype) for n, p in model.named_parameters() if p.requires_grad]
     
-    print(f"\n[INFO] After *trainer = QwenSFTTrainer(
-        model=model,
-        processing_class=processor,
-        args=training_args,
-        **data_module
-    )* Trainable parameter count: {len(trainable_params)}")
+    print(f"\n[INFO] After trainer Trainable parameter count: {len(trainable_params)}")
     
     if list(pathlib.Path(training_args.output_dir).glob("checkpoint-*")):
         trainer.train(resume_from_checkpoint=True)
