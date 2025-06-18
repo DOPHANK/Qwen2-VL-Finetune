23f4028fb0517850839fead72c17d48c9a56a8a4
DOPHANK <93715124+DOPHANK@users.noreply.github.com>
2025-06-18 11:34:10 +0700

    Update train_sft.py

--- src/train/train_sft.py
+++ src/train/train_sft.py
@@ -67,6 +67,10 @@ def train():
     
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
 
+    rank0_print(model_args)
+    rank0_print(data_args)
+    rank0_print(training_args)
+    
     rank0_print(f"Activate liger: {training_args.use_liger}")
     use_liger = training_args.use_liger
     if "Qwen2.5" in model_args.model_id:
