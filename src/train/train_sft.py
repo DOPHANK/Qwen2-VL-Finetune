8f4d9b4f8b3ed36e05498afef2192d5aa18b1dc0
DOPHANK <93715124+DOPHANK@users.noreply.github.com>
2025-06-23 14:09:32 +0700

    Update train_sft.py

--- src/train/train_sft.py
+++ src/train/train_sft.py
@@ -286,9 +286,7 @@ def train():
         trainer.train()
         
     # ✅ metrics with generated output
-    predictions = trainer.predict(data_module.eval_dataset)
     metrics = trainer.evaluate(predict_with_generate=True)
-    rank0_print("Final predictions:", predictions)
     rank0_print("Final metrics:", metrics)
 
     trainer.save_state()
