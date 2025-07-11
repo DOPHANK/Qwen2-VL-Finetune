3700753075d1138ea56d4e48122989dff11ec215
DOPHANK <93715124+DOPHANK@users.noreply.github.com>
2025-07-11 11:35:55 +0700

    Update finetune.sh

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -24,6 +24,7 @@ python3 -c "import torch; torch.cuda.empty_cache()"
 
 cd /kaggle/working/Qwen2-VL-Finetune/
 ls -F
+ls /vital_vision/images/
 
 # Run training
 accelerate launch src/train/train_sft.py \
