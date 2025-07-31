9216e6a8f17136b5d3fb7dccc9e57348d4eca28c
DOPHANK <93715124+DOPHANK@users.noreply.github.com>
2025-07-31 11:19:02 +0700

    Update train_sft.py

--- src/train/train_sft.py
+++ src/train/train_sft.py
@@ -363,8 +363,7 @@ def train():
                     },
                     {
                         "role": "assistant",
-                        "content": "<im_start>Side Code: 1<im_end>\n<im_start>ID: 8<im_end>\n<im_start>RECORD_DTC: 20/11/2024 00:00:00<im_end>\n<im_start>SEX: Male<im_end>\n<im_start>AGE: 28.0<im_end>\n<im_start>ADMISSION_DTC: 05/09/2019 00:00:00<im_end>\n<im_start>DISCHARGE_DTC: 13/09/2019 00:00:00<im_end>\n<im_start>ILLNESS_DAYS: 8.0<im_end>\n<im_start>TEMP_ADM: 38.1<im_end>\n<im_start>SYSBP: 117.0<im_end>\n<im_start>DIABP: 62.0<im_end>\n<im_start>HR: 142.0<im_end>\n<im_start>RESP: 34.0<im_end>\n<im_start>SPO2: 100.0<im_end>\n<im_start>CONSCIOUS_LEVEL: Unconscious<im_end>\n<im_start>WEIGHT: nan<im_end>\n<im_start>NA_W: True<im_end>\n<im_start>HEIGHT: nan<im_end>\n<im_start>NA_H: True<im_end>\n<im_start>HYPERTENSION: N<im_end>\n<im_start>DIABETES: N<im_end>\n<im_start>DYSLIPIDAEMIA: N<im_end>\n<im_start>IHD: N<im_end>\n<im_start>CLUNGD: N<im_end>\n<im_start>CVD: N<im_end>\n<im_start>CLIVERD: N<im_end>\n<im_start>CKD: N<im_end>\n<im_start>MALIGNANCY: N<im_end>\n<im_start>AUTOIMMUNE_DISEASE: N<im_end>\n<im_start>OTH_MORBIDITIES: nan<im_end>"
-                    },
+                        "content": "<im_start>Side Code: 1<im_end>\n<im_start>ID: 8<im_end>\n<im_start>RECORD_DTC: 20/11/2024<im_end>\n...},
                     # Separator
                     {
                         "role": "system",
@@ -375,7 +374,11 @@ def train():
                         "role": "user",
                         "content": [
                             {"type": "image", "image": test_image},
-                            {"type": "text", "text": "Extract infos from this new image, as KEY: VALUE pairs in ChatML format.\nUse <im_start> and <im_end> to wrap each key-value pair like this:\n<im_start>KEY: VALUE<im_end>\n Reminder that checkboxes are at front of VALUEs."}
+                            {"type": "text", "text": "Extract infos from this new image, as KEY: VALUE pairs in ChatML format."
+                                                    "Use <im_start> and <im_end> to wrap each key-value pair like this:"
+                                                    "<im_start>KEY: VALUE<im_end>"
+                                                    "Reminder that checkboxes are at front of VALUEs."
+                            }
                         ]
                     }
                 ]
